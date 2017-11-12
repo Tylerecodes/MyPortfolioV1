de25c7b0b311dd512af4cd6847979d241c95a50e
Tyler Estes <tyler@tylerecodes.com>
2017-11-11 23:28:00 -0600

    Updated Navbar with transition and scrollspy

--- js/source.js
+++ js/source.js
@@ -1,10 +1,18 @@
+
+//Transitioning Navbar with Page-scroll//
 $(document).ready(function() {
 	$(window).scroll(function() {
   	if($(document).scrollTop() > 10) {
-    	$('.navbar').addClass('shrink');
+        $('.navbar').addClass('shrink');
+        $('.collapse').addClass('shrink');
     }
     else {
     $('.navbar').removeClass('shrink');
+    $('.collapse').removeClass('shrink');
     }
   });
 });
+
+
+//Scrollspy on navbar while scrolling//
+$('body').scrollspy({ target: '#navbar' })
