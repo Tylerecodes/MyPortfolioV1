47754538481f6fa21c63da8c17eb8da04c510925
Tyler Estes <tyler@tylerecodes.com>
2017-11-19 20:24:45 -0600

    Mailer update

--- PHPMailer/index.php
+++ PHPMailer/index.php
@@ -1,35 +1,26 @@
 <?php
     $msg = "";
 	use PHPMailer\PHPMailer\PHPMailer;
-	include_once "PHPMailer/PHPMailer.php";
-	include_once "PHPMailer/Exception.php";
-	include_once "PHPMailer/SMTP.php";
+	include_once "PHPMailer.php";
+	include_once "Exception.php";
 
 	if (isset($_POST['submit'])) {
 		$subject = $_POST['name'];
 		$email = $_POST['email'];
 		$message = $_POST['message'];
 
-		//if we want to send via SMTP
-       /*  $mail->Host = "smtp.gmail.com";
-        $mail->isSMTP();
-        $mail->SMTPAuth = true;
-        $mail->Username = "gte043@gmail.com";
-        $mail->Password = "impress.chant.wanting";
-        $mail->SMTPSecure = "ssl"; //TLS
-        $mail->Port = 465; //587 */
-
+        $mail = new PHPMailer();
 		$mail->addAddress('tyler@tylerecodes.com');
 		$mail->setFrom($email);
 		$mail->Subject = $subject;
 		$mail->isHTML(true);
 		$mail->Body = $message;
 
-		if ($mail->send())
-		    $msg = "Your email has been sent, thank you!";
-		else
-		    $msg = "Please try again!";
-
-		unlink($file);
+        //send the message, check for errors
+        if (!$mail->send()) {
+            echo "Mailer Error: " . $mail->ErrorInfo;
+        } else {
+            echo "Message sent!";
+        }
 	}
 ?>
