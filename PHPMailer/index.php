74214cbe7b2f2ed2470dca5784db8e53fe809044
Tyler Estes <tyler@tylerecodes.com>
2017-11-19 22:17:54 -0600

    Contact page works! Cleaning Code

--- PHPMailer/index.php
+++ PHPMailer/index.php
@@ -4,23 +4,21 @@
 	include_once "PHPMailer.php";
 	include_once "Exception.php";
 
-	if (isset($_POST['submit'])) {
-		$subject = $_POST['name'];
-		$email = $_POST['email'];
-		$message = $_POST['message'];
+	$subject = $_POST['name'];
+	$email = $_POST['email'];
+	$message = $_POST['message'];
 
-        $mail = new PHPMailer();
-		$mail->addAddress('tyler@tylerecodes.com');
-		$mail->setFrom($email);
-		$mail->Subject = $subject;
-		$mail->isHTML(true);
-		$mail->Body = $message;
+	$mail = new PHPMailer();
+	$mail->addAddress('tyler@tylerecodes.com');
+	$mail->setFrom($email);
+	$mail->Subject = $subject;
+	$mail->isHTML(true);
+	$mail->Body = $message;
 
-        //send the message, check for errors
-        if (!$mail->send()) {
-            echo "Mailer Error: " . $mail->ErrorInfo;
-        } else {
-            echo "Message sent!";
-        }
+	//send the message, check for errors
+	if (!$mail->send()) {
+		echo "Mailer Error: " . $mail->ErrorInfo;
+	} else {
+		echo "Message sent!";
 	}
 ?>
